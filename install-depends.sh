55979c1fa549038eff5e26cea9f2507431f8e252
Timm <timm.hayer@googlemail.com>
2014-07-22 19:16:10 +1000

    Update install-depends.sh

--- install-depends.sh
+++ install-depends.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get install python-dev python-setuptools python-pip
 sudo pip install flask
+sudo pip install wiringpi2
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 ./build
@@ -9,4 +10,4 @@ git clone https://github.com/Gadgetoid/WiringPi2-Python
 cd WiringPi2-Python/
 sudo python setup.py install
 cd ../
-sudo ./piglow.py
+sudo python ./piglow.py
